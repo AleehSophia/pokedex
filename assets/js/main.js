d195e7b34aa0ca992701d7b47e46d40e9ed2d7f9
AleehSophia <alessia.casado@gmail.com>
2022-12-01 19:03:02 -0300

    ultimas atualizacoes tentando abrir pagina de detalhe

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,33 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const detailButton = document.getElementById('detailButton')
+const pkDt = document.getElementById('pkDt')
+const showPoke = document.querySelector('.content-poke')
+const sectionPoke = document.querySelector('.content')
+
 const limit = 5
 let offset = 0
 
+function pokeDetail(pokemon) {
+    return `
+        <div class="content-poke">
+            <div id="poke-id" ${pokemon.type}">
+                <span class="poke-number">#${pokemon.number}</span>
+                <span class="poke-name">${pokemon.name}</span>
+                <div class="poke-detail">
+                    <div class="poke-img ${pokemon.type}">
+                        <img src="${pokemon.photo}"
+                        alt="${pokemon.name}">
+                    </div>    
+                </div>
+            </div>
+        </div>
+    `
+}
+
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li id="pokemon" class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
@@ -15,6 +37,9 @@ function convertPokemonToLi(pokemon) {
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
             </div>
+            <button onclick="getDetails(event)" id="detailButton" type="button">
+                Pokemon detail
+            </button>
         </li>
     `
 }
@@ -32,3 +57,11 @@ loadMoreButton.addEventListener('click', () => {
     offset += limit
     loadPokemonItens(offset, limit)
 })
+
+function getDetails(event) {
+    pokeApi.getDetails(event).then((pokemon = []) => {
+        const newHtml = pokeDetail(pokemon)
+        showPoke.innerHTML += newHtml
+        sectionPoke.classList.add('open-poke')
+    })
+}
